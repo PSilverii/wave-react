e75c1d12d8c02aaf11aa11c0e45c04facce0c0a1
Patricio Silverii <silveriideveloper@gmail.com>
2022-08-28 15:20:49 -0300

    menu-e-commers

---
+++ src/components/NavBar.js
@@ -0,0 +1,140 @@
+//fotos
+import logoXs from "../app/assets/logo/logo-xs.png"
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <ul className="header__account">
+                    <li className="header__account--align">
+                        <a className="header__account--btn" href="#">Crear Cuenta</a>
+                    </li>
+                    <li className="header__account--align">
+                        <a className="header__account--btn" href="#">Iniciar Sesión</a>
+                    </li>
+                </ul>
+                <section className="nav__mobile">
+                    <label for="check__mobile">
+                        <p className="nav__btn__mobile--icon">menu</p>
+                    </label>
+                    <input type="checkbox" id="check__mobile" />
+                    <ul className="nav__menu__mobile">
+                        <li>
+                            <a class="active" href="#">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="#">Pantalones</a>
+                        </li>
+                        <li>
+                            <a href="#">Remeras</a>
+                        </li>
+                        <li>
+                            <a href="#">Buzos</a>
+                        </li>
+                        <li>
+                            <a href="#">Zapatillas</a>
+                        </li>
+                        <li>
+                            <a href="#">Neoprene</a>
+                        </li>
+                        <li>
+                            <a href="#">Botas Neoprene</a>
+                        </li>
+                        <li>
+                            <a href="#">Lycras</a>
+                        </li>
+                        <li>
+                            <a href="#">Fundas para tablas de Surf</a>
+                        </li>
+                        <li>
+                            <a href="#">Contactanos</a>
+                        </li>
+                    </ul>
+                    <div className="nav__btn__mobile">
+                        <label for="check__search__mobile">
+                            <p className="nav__btn__mobile--icon no__margin">search</p>
+                        </label>
+                        <input type="checkbox" id="check__search__mobile" />
+                        <ul className="nav__btn__search__mobile">
+                            <li>
+                                <input className="nav__btn__search__mobile--input" type="text" placeholder="Buscar" />
+                            </li>
+                        </ul>
+                        <label for="check__cart__mobile">
+                            <p className="nav__btn__mobile--icon">shopping_cart</p>
+                        </label>
+                        <input type="checkbox" id="check__cart__mobile" />
+                        <ul className="nav__btn__cart__mobile">
+                            <li>
+                                <p>Aún no tienes nada en tu carrito</p>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <section className="nav__desktop">
+                    <picture className="nav__desktop--logo">
+                        <img src={logoXs} alt="logo" />
+                    </picture>
+                    <ul className="nav__desktop__btn">
+                        <li className="nav__desktop__btn--align">
+                            <a className="nav__desktop__btn--text" href="#">Home</a>
+                        </li>
+                        <li className="nav__desktop__btn--text nav__desktop__btn--align">
+                            <label className="nav__desktop__btn--pointer" for="check__desktop">Tienda Online</label>
+                            <input type="checkbox" id="check__desktop" />
+                            <ul className="nav__desktop__menu">
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Pantalones</a>
+                                </li>
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Remeras</a>
+                                </li>
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Buzos</a>
+                                </li>
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Zapatillas</a>
+                                </li>
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Neoprene</a>
+                                </li>
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Botas Neoprene</a>
+                                </li>
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Lycras</a>
+                                </li>
+                                <li>
+                                    <a className="nav__desktop__menu--text" href="#">Fundas para tablas de Surf</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className="nav__desktop__btn--align"><a className="nav__desktop__btn--text" href="#">Contactanos</a></li>
+                    </ul>
+                    <div className="nav__btn__desktop">
+                        <label for="check__search__desktop">
+                            <p className="nav__btn__desktop--icon no__margin">search</p>
+                        </label>
+                        <input type="checkbox" id="check__search__desktop" />
+                        <ul className="nav__btn__search__desktop">
+                            <li>
+                                <input className="nav__btn__search__desktop--input" type="text" placeholder="Buscar" />
+                            </li>
+                        </ul>
+                        <label for="check__cart__desktop">
+                            <p className="nav__btn__desktop--icon">shopping_cart</p>
+                        </label>
+                        <input type="checkbox" id="check__cart__desktop" />
+                        <ul className="nav__btn__cart__desktop">
+                            <li>
+                                <p>Aún no tienes nada en tu carrito</p>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
